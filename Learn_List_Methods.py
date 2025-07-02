2bb3612a9a2fff92b19697c32f55fd878544c697
stepback269 <ggimlan@yahoo.com>
2025-07-02 01:51:54 -0700

    Add files via upload
    Inserted skip function into tutorial number 01

--- Learn_List_Methods.py
+++ Learn_List_Methods.py
@@ -1,8 +1,18 @@
-# Date: 7/01a/2025  version 004  <-- latest edit search for: "#: 03:"  or for "#: 53:"
-# This Python file will be similar to the earlier Learn_String_Methods and also base don Indently's YouTube video
-# (1) located at: https://www.youtube.com/watch?v=0yySumZTxJ0&t=1s
+# Date: 7/02b/2025  version 004a  <-- latest edit search for: "#: 03:"  or for "#: 53:"
+# (1) This Python file will be similar to the earlier Learn_String_Methods and also base don Indently's YouTube video
+#   (1a) located at: https://www.youtube.com/watch?v=0yySumZTxJ0&t=1s
+# (2) Emphasis is diverted here away from the target of the "learning" (e.g. List methods) and toward the
+#     "LEARNING" itself.
+#   (2a) According to a number of educators, stopping to ask and answer QUESTIONS about what you are studying
+#       increases the effectiveness of the learning process significantly. (Because it forces your brain to
+#       actively generate its own output rather than passively intaking information).
+#   (2b) See "Smartest People Read Books Like This" by Python Programmer here: https://www.youtube.com/watch?v=mOJu1I57Ajc
+#   (2c) See "Semantic Encoding" here: https://www.google.com/search?q=smantic+encoding&sourceid=chrome&ie=UTF-8
+#   (2d) See "Prompt Engineering" here: https://www.youtube.com/watch?v=_ZvnD73m40o
+#   (2e) See "Learning resources are everywhere" here: https://gilesknowledge.substack.com/p/learning-resources-are-everywhere
+# (3) In the coding exercises below we will include a FOOD FOR THOUGHT questionire to improve the "LEARNING" aspects
 
-# There are only 11 list methods according to Indently --NOT counting Dunder methods
+# According to Indently, there are only 11 list methods --NOT counting Dunder methods
 # 38:APPEND(), 39:CLEAR(),   40:COPY(), 41:COUNT(),  42:EXTEND()
 # 43:INDEX(),  44:INSERT(),  45:POP(),  46:REMOVE(), 47:REVERSE(), 48:SORT()
 
@@ -10,8 +20,8 @@
 # 01:CHOICE(), 02:SHUFFLE(), 03:RANDOMrange(), more ...
 
 # Aside from Indently, there are other worthwhile videos to cover here including:
-# (2) Mosh --- How to Use Lists in Python: https://www.youtube.com/watch?v=9OeznAkyQz4
-# (3) Bro Code -- Python lists, sets, and tuples explained: https://www.youtube.com/watch?v=gOMW_n2-2Mw
+# (4) Mosh --- How to Use Lists in Python: https://www.youtube.com/watch?v=9OeznAkyQz4
+# (5) Bro Code -- Python lists, sets, and tuples explained: https://www.youtube.com/watch?v=gOMW_n2-2Mw
 
 '''
 FLOW CONTROL CHANGES:
@@ -21,7 +31,7 @@ skip_diz_func_B = a second list of 30 integers, each for controlling activation 
 skip_diz_func_C = a third  list of 30 integers, each for controlling activation of tutorials #60 to #99
 skip_diz_template = range(0,31) = use this as a template for identifying the index of the 30 int lists
 '''
-def gen_index_templ4(numb_indices):      #<--func prints out a template of numb number of indices plus one
+def gen_index_templ4(numb_indices):      #--func prints out a template of numb number of indices plus one
     skip_diz_template_create = range(numb_indices + 1)
     skip_diz_template = list(skip_diz_template_create)
     for i in skip_diz_template: print(format(i ,"02d"), end = ' ')
@@ -33,38 +43,55 @@ gen_index_templ4(10)
 # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 # see https://www.geeksforgeeks.org/python/range-to-a-list-in-python/
 
-def gen_preFilled_list4(numb_indices, filler = 0):    #<--func prints out a list of numb number of indices plus one
-    skip_diz_template_A = [filler] * (numb_indices +1)    #<-- and returns the gen'd list
+def gen_preFilled_list4(numb_indices, filler = 0):    #--func prints out a list of numb number of indices plus one
+    skip_diz_template_A = [filler] * (numb_indices +1)    #-- and returns the gen'd list
     for i in skip_diz_template_A: print(format(i ,"02d"), end = ' ')
     print(f'\nPre-filled list printed out above for {numb_indices} plus one indices\n')
     return skip_diz_template_A
-skip_diz_list = gen_preFilled_list4(6, 0)
-skip_diz_list = gen_preFilled_list4(10, 1)
-print(skip_diz_list)
-numb_indices = 10   #<--this value is internal to the last run of function above
-print(f'Pre-filled list printed out above for {numb_indices} plus one indices\n')
 
-# breakpoint()
-# 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
-# 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+skip_diz_list_a = gen_preFilled_list4(6, 0)
+skip_diz_list_b = gen_preFilled_list4(10, 1)
+print(skip_diz_list_a)
+print(skip_diz_list_b)
+
+numb_indices = 10   #--this value is internal to the last run of function above
+print(f'Pre-filled strings list printed out above for {numb_indices} plus one indices\n')
 
 # problem1: first 10 positions are 1 digit and then after it is two digits
 # see https://stackoverflow.com/questions/29000613/add-leading-zeros-to-a-list-of-numbers-in-python
 
-# breakpoint()
 # problem2: how to align items on two rows of print outs (now that we're doing row1 + row2 for die dots)
 # see https://labex.io/tutorials/python-how-to-align-output-in-python-printing-418802
+
+def halt4_food(method_ID_numb = 0, method_name = 'List method', verb = 'generate', output = 'modified_list',
+               original = 'original_list_example'):
+    esc = '\033['; wht_text = esc + '37m'; yel_text = esc + '33m'; red_text = esc + '31m'
+    print(f'\n#0{method_ID_numb}: Output of {method_name} is this {verb}d \t{output}')
+    print(f' \t\t\t\t original list was = \t\t{original}\n')
+    print(f'{yel_text}FOOD FOR THOUGHT: What applications can {method_name} be used for?{wht_text}')
+    print(f'(1): {method_name} can {verb} a ____ list to produce ____ in context of ____')
+    print(f'(2): {method_name} can {verb} a ____ list to produce ____ in context of ____')
+    halt_02 = input(f'Do you have any more application ideas for {red_text}{method_name}{wht_text} ??__\n')
 
 print('WELCOME TO LEARN LIST METHODS')
 print('-' * 20)
 print(f'First tutorial is number #: 01: CHOICE()')
+
 method_name = 'CHOICE()'
 method_ID_numb = 1
+list_example_01 = ['Adam', 'Brad', 'Charlie', 'David', 'Ernie']
+list_example_01_mod = ['Brad', 'Charlie', 'David']
+halt4_food(method_ID_numb, method_name, 'Random choosing',list_example_01_mod,
+               list_example_01)
+# breakpoint()
+
 
 #: 01:
 # Indently's code for "gluing"-in a random choice function ( https://www.youtube.com/watch?v=H6Zn8MdcBwg )
-from random import choice       # ^^^^ above URL explains advantages of the Python language
-# dice_faces: int = range(1,7)    #<-- generates a list of integers 1-6 (default start of range is 0)
+method_name = 'CHOICE()'; method_ID_numb = 1; verb= 'Choosing dice roll outcome'
+
+from random import choice       # ^^^^ above Indently URL explains advantages of the Python language
+# dice_faces: int = range(1,7)    #-- generates a list of integers 1-6 (default start of range is 0)
 # go_again = 'y'                  # ^^^ forinfo re random.choice, see:
 #                               # https://www.google.com/search?q=python+random+choice&sourceid=chrome&ie=UTF-8
 # face_imgs = ['🎲', '⛹️‍♀️', '🧑🏻‍🤝‍🧑🏻', '👪', '👨‍👨‍👧‍👦', '👪👨‍👦', '👪👪']  #<-- icons were obtained from below URL:
@@ -73,24 +100,34 @@ from random import choice       # ^^^^ above URL explains advantages of the Pyth
 # ^^^^ https://copychar.cc
 #   00 01 02 03 04 05 06
 #   Template printed out above for 6 plus one indices
-face_imgs_row1 = ['🤬', '🧐', '🤓🤓', '😲😲', '🤩🤩', '😋   🤪',   '🤠🤠🤠' ]
-face_imgs_row2 = ['☠️', ' ',   '  ',   '😲',    '🤩🤩', '😋😛🤪',   '🤠🤠🤠' ]
-print('\n\n', '-' * 20)
 
-dice_faces: int = range(1,7)    #<-- generates a list of integers 1-6 (default start of range is 0)
-go_again = 'y'
-while go_again == 'y':
-    roll_1 = choice(dice_faces)
-    roll_2 = choice(dice_faces)
-    print(f'\n#:00; example of random choice')
-    print(f' 1st die roll gives you a \t{roll_1}\t{face_imgs_row1[roll_1]}')
-    print(f' \t\t\t\t\t\t\t\t{face_imgs_row2[roll_1]}\n')
-    print(f' 2nd die roll gives you a \t{roll_2}\t{face_imgs_row1[roll_2]}')
-    print(f' \t\t\t\t\t\t\t\t{face_imgs_row2[roll_2]}\n')
-    print(f' The SUM of the two rolls is \t{roll_1 + roll_2} \n')
-    go_again = input('Try again? Enter y or other here__')
+def choice_of_die_outcomes(method_ID_numb, method_name, verb, skip_diz_list_a):
+    if skip_diz_list_a[method_ID_numb] == 1:
+        print(f'method number {method_ID_numb} has been skipped\n')
+        return None     #--skip this tutorial if skip position is set to 1
+    face_imgs_row1 = ['🤬', '🧐', '🤓🤓', '😲😲', '🤩🤩', '😋   🤪',   '🤠🤠🤠' ]
+    face_imgs_row2 = ['☠️', ' ',   '  ',   '😲',    '🤩🤩', '😋😛🤪',   '🤠🤠🤠' ]
+    print('\n\n', '-' * 20)
+
+    dice_faces: int = range(1,7)    #-- generates a list of integers 1-6 (default start of range is 0)
+    go_again = 'y'
+    while go_again == 'y':
+        roll_1 = choice(dice_faces)
+        roll_2 = choice(dice_faces)
+        print(f'\n#:00; example of random choice')
+        print(f' 1st die roll gives you a \t{roll_1}\t{face_imgs_row1[roll_1]}')
+        print(f' \t\t\t\t\t\t\t\t{face_imgs_row2[roll_1]}\n')
+        print(f' 2nd die roll gives you a \t{roll_2}\t{face_imgs_row1[roll_2]}')
+        print(f' \t\t\t\t\t\t\t\t{face_imgs_row2[roll_2]}\n')
+        print(f' The SUM of the two rolls is \t{roll_1 + roll_2} \n')
+        go_again = input('Try again? Enter y or other here__')
+    halt4_food(method_ID_numb, method_name, verb, face_imgs_row1,
+               face_imgs_row2)
+
+choice_of_die_outcomes(method_ID_numb, method_name, verb, skip_diz_list_a)
 
 breakpoint()
+
 #: 02a ANSI COLOR escape codes
 esc = '\033['
 wht_text = esc + '37m'; yel_text = esc + '33m'; red_text = esc + '31m'; grn_text = esc + '32m'
